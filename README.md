76b04351dc75782471a5f8673497e4536ff0a9fd
Tanvir A <tanvirwebexpert@gmail.com>
2023-10-08 15:41:00 +0600

    Added MomentJS for display current date

--- README.md
+++ README.md
@@ -3,3 +3,4 @@
 1. Installed React Route, Tailwind CSS, Daisy UI, Firebase SDK
 2. Folder Structure and Router setup
 3. Added shared components
+4. Added MomentJS for display current date
--- src/shared/Header/Header.jsx
+++ src/shared/Header/Header.jsx
@@ -1,7 +1,11 @@
+import moment from "moment/moment";
+import Logo from "../../assets/logo.png";
 const Header = () => {
     return (
-        <div>
-            Header
+        <div className="text-center">
+            <img className="mx-auto" src={Logo} alt="" />
+            <p>Journalism Without Fear or Favour</p>
+            <p>{moment().format("dddd, MMMM D, YYYY")}</p>
         </div>
     );
 };
