5d8cab1e63a3269fe6ceabc4e4c5493a9fb23187
Tanvir A <tanvirwebexpert@gmail.com>
2023-10-08 18:45:25 +0600

    Displayed dynamic category on the right sidebar

--- src/shared/LeftSideBar/LeftSideBar.jsx
+++ src/shared/LeftSideBar/LeftSideBar.jsx
@@ -1,7 +1,24 @@
+import { useEffect, useState } from "react";
+import { Link } from "react-router-dom";
+
 const LeftSideBar = () => {
+    const [categories, setCategories] = useState([]);
+    useEffect(() => {
+        fetch("categories.json")
+        .then(res => res.json())
+        .then(data => setCategories(data))
+    }, [])
+
     return (
         <div>
-            Left side bar
+            <div className="space-y-3 border p-4 mb-4">
+                <h2 className="font-extrabold text-2xl">Q Zone</h2>
+                <ul className="space-y-2">
+                    {
+                        categories.map(category => <li key={category.id}><Link to={`/category/${category.id}`}>{category.name}</Link></li>)
+                    }
+                </ul>
+            </div>
         </div>
     );
 };
